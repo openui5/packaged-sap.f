1127f807967478d36cd487251529e91c494bffd4
OpenUI5 <openui5@sap.com>
2016-11-22 09:48:10 +0100

    1.42.6

--- resources/sap/f/DynamicPageTitle.js
+++ resources/sap/f/DynamicPageTitle.js
@@ -44,7 +44,7 @@ sap.ui.define(["jquery.sap.global", "./library", "sap/ui/core/Control", "sap/m/O
 	 * @extends sap.ui.core.Control
 	 *
 	 * @author SAP SE
-	 * @version 1.42.5
+	 * @version 1.42.6
 	 *
 	 * @constructor
 	 * @public

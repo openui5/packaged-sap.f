7cce8fb53ac7f26befadc535ed01bc3999ae3c9b
OpenUI5 <openui5@sap.com>
2017-01-18 11:41:42 +0100

    1.42.9

--- resources/sap/f/library.js
+++ resources/sap/f/library.js
@@ -19,14 +19,14 @@ sap.ui.define(['jquery.sap.global',
 	 * @namespace
 	 * @name sap.f
 	 * @author SAP SE
-	 * @version 1.42.8
+	 * @version 1.42.9
 	 * @public
 	 */
 
 	// delegate further initialization of this library to the Core
 	sap.ui.getCore().initLibrary({
 		name : 'sap.f',
-		version: '1.42.8',
+		version: '1.42.9',
 		dependencies : ['sap.ui.core','sap.m'],
 		types: [
 			"sap.f.ThreeColumnLayoutType"

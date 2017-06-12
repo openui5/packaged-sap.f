439feb6ff542ae66541ef1989e316ca4963580fa
OpenUI5 <openui5@sap.com>
2017-06-12 18:36:22 +0200

    1.48.2

--- resources/sap/f/semantic/PositiveAction.js
+++ resources/sap/f/semantic/PositiveAction.js
@@ -19,7 +19,7 @@ sap.ui.define(['sap/f/semantic/SemanticButton'], function(SemanticButton) {
 	* @extends <code>sap.f.semantic.SemanticButton</code>
 	*
 	* @author SAP SE
-	* @version 1.48.1
+	* @version 1.48.2
 	*
 	* @constructor
 	* @public

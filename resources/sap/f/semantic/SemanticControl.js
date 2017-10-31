865a585c0355aa3d0c14a03be3ddd1c08cbc1b09
OpenUI5 <openui5@sap.com>
2017-10-31 14:47:47 +0100

    1.50.5

--- resources/sap/f/semantic/SemanticControl.js
+++ resources/sap/f/semantic/SemanticControl.js
@@ -25,7 +25,7 @@ sap.ui.define([
 	* @abstract
 	*
 	* @author SAP SE
-	* @version 1.50.4
+	* @version 1.50.5
 	*
 	* @constructor
 	* @public
--- resources/sap/f/semantic/SemanticToggleButton.js
+++ resources/sap/f/semantic/SemanticToggleButton.js
@@ -27,7 +27,7 @@ sap.ui.define([
 	* @abstract
 	*
 	* @author SAP SE
-	* @version 1.50.4
+	* @version 1.50.5
 	*
 	* @constructor
 	* @public

c5e659b211f0e5828a1a04bb0c8b8454570b8426
OpenUI5 <openui5@sap.com>
2017-12-18 19:01:33 +0100

    1.52.3

--- resources/sap/f/semantic/CopyAction.js
+++ resources/sap/f/semantic/CopyAction.js
@@ -18,7 +18,7 @@ sap.ui.define(['./SemanticButton'], function(SemanticButton) {
 	* @extends sap.f.semantic.SemanticButton
 	*
 	* @author SAP SE
-	* @version 1.52.2
+	* @version 1.52.3
 	*
 	* @constructor
 	* @public
--- resources/sap/f/semantic/DeleteAction.js
+++ resources/sap/f/semantic/DeleteAction.js
@@ -19,7 +19,7 @@ sap.ui.define(['sap/f/semantic/SemanticButton'], function(SemanticButton) {
 	* @extends sap.f.semantic.SemanticButton
 	*
 	* @author SAP SE
-	* @version 1.52.2
+	* @version 1.52.3
 	*
 	* @constructor
 	* @public

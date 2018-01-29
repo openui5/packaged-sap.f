d68a2103ee8434928794b51e1706bd9c05466677
OpenUI5 <openui5@sap.com>
2018-01-29 23:39:30 +0100

    1.52.5

--- resources/sap/f/semantic/SemanticConfiguration.js
+++ resources/sap/f/semantic/SemanticConfiguration.js
@@ -30,7 +30,7 @@ sap.ui.define([
 	* @class
 	* Defines the visual properties and placement for each supported semantic type.
 	*
-	* @version 1.52.4
+	* @version 1.52.5
 	* @private
 	* @since 1.46.0
 	* @alias sap.f.semantic.SemanticConfiguration

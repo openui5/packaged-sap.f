21f61071823611d8273a0ef27339080205054a70
OpenUI5 <openui5@sap.com>
2018-08-13 17:51:03 +0200

    1.56.6

--- resources/sap/f/semantic/AddAction.js
+++ resources/sap/f/semantic/AddAction.js
@@ -18,7 +18,7 @@ sap.ui.define(['sap/f/semantic/SemanticButton'], function(SemanticButton) {
 	* @extends sap.f.semantic.SemanticButton
 	*
 	* @author SAP SE
-	* @version 1.56.5
+	* @version 1.56.6
 	*
 	* @constructor
 	* @public
--- resources/sap/f/semantic/FlagAction.js
+++ resources/sap/f/semantic/FlagAction.js
@@ -19,7 +19,7 @@ sap.ui.define(['./SemanticToggleButton'], function(SemanticToggleButton) {
 	* @extends sap.f.semantic.SemanticToggleButton
 	*
 	* @author SAP SE
-	* @version 1.56.5
+	* @version 1.56.6
 	*
 	* @constructor
 	* @public

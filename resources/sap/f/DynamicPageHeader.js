b35bbe0ac1078efd6b7d304692bd14bed858f160
OpenUI5 <openui5@sap.com>
2018-09-17 17:18:01 +0200

    1.52.19

--- resources/sap/f/DynamicPageHeader.js
+++ resources/sap/f/DynamicPageHeader.js
@@ -48,7 +48,7 @@ sap.ui.define(["./library",
 		 * @extends sap.ui.core.Control
 		 *
 		 * @author SAP SE
-		 * @version 1.52.18
+		 * @version 1.52.19
 		 *
 		 * @constructor
 		 * @public
--- resources/sap/f/semantic/ShareInJamAction.js
+++ resources/sap/f/semantic/ShareInJamAction.js
@@ -19,7 +19,7 @@ sap.ui.define(['sap/f/semantic/SemanticButton'], function(SemanticButton) {
 	* @extends sap.f.semantic.SemanticButton
 	*
 	* @author SAP SE
-	* @version 1.52.18
+	* @version 1.52.19
 	*
 	* @constructor
 	* @public

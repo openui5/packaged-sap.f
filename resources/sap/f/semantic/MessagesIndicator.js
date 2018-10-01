ec55f062c165abea89da51bacb2ea2458a5253b7
OpenUI5 <openui5@sap.com>
2018-10-01 15:43:08 +0200

    1.56.11

--- resources/sap/f/semantic/MessagesIndicator.js
+++ resources/sap/f/semantic/MessagesIndicator.js
@@ -19,7 +19,7 @@ sap.ui.define(['./SemanticButton'], function(SemanticButton) {
 	* @extends sap.f.semantic.SemanticButton
 	*
 	* @author SAP SE
-	* @version 1.56.10
+	* @version 1.56.11
 	*
 	* @constructor
 	* @public

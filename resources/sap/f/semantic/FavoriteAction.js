beb59f3240a9cde2dfd45f5cd48eb2b913ab9cd0
OpenUI5 <openui5@sap.com>
2018-10-26 15:21:17 +0200

    1.58.5

--- resources/sap/f/semantic/FavoriteAction.js
+++ resources/sap/f/semantic/FavoriteAction.js
@@ -19,7 +19,7 @@ sap.ui.define(['./SemanticToggleButton'], function(SemanticToggleButton) {
 	* @extends sap.f.semantic.SemanticToggleButton
 	*
 	* @author SAP SE
-	* @version 1.58.4
+	* @version 1.58.5
 	*
 	* @constructor
 	* @public
--- resources/sap/f/semantic/MessagesIndicator.js
+++ resources/sap/f/semantic/MessagesIndicator.js
@@ -19,7 +19,7 @@ sap.ui.define(['./SemanticButton'], function(SemanticButton) {
 	* @extends sap.f.semantic.SemanticButton
 	*
 	* @author SAP SE
-	* @version 1.58.4
+	* @version 1.58.5
 	*
 	* @constructor
 	* @public

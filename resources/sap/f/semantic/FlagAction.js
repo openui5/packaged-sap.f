5bb85629895e26e8577dabffeb58a01c04fbdd50
OpenUI5 <openui5@sap.com>
2019-10-19 19:40:51 +0200

    1.52.35

--- resources/sap/f/semantic/FlagAction.js
+++ resources/sap/f/semantic/FlagAction.js
@@ -19,7 +19,7 @@ sap.ui.define(['./SemanticToggleButton'], function(SemanticToggleButton) {
 	* @extends sap.f.semantic.SemanticToggleButton
 	*
 	* @author SAP SE
-	* @version 1.52.34
+	* @version 1.52.35
 	*
 	* @constructor
 	* @public

3e010fefbbf81ddc8e7eafe5c24e4c5859b11f7a
OpenUI5 <openui5@sap.com>
2019-10-30 09:55:29 +0100

    1.52.36

--- resources/sap/f/semantic/SemanticButton.js
+++ resources/sap/f/semantic/SemanticButton.js
@@ -25,7 +25,7 @@ sap.ui.define([
 	* @abstract
 	*
 	* @author SAP SE
-	* @version 1.52.35
+	* @version 1.52.36
 	*
 	* @constructor
 	* @public
--- resources/sap/f/semantic/TitleMainAction.js
+++ resources/sap/f/semantic/TitleMainAction.js
@@ -19,7 +19,7 @@ sap.ui.define(["./MainAction"], function(MainAction) {
 	* @extends sap.f.semantic.MainAction
 	*
 	* @author SAP SE
-	* @version 1.52.35
+	* @version 1.52.36
 	*
 	* @constructor
 	* @public

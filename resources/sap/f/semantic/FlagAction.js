fb16feaf809bad0a25a46530987756898cf89699
OpenUI5 <openui5@sap.com>
2020-07-13 16:15:59 +0200

    1.52.42

--- resources/sap/f/semantic/FlagAction.js
+++ resources/sap/f/semantic/FlagAction.js
@@ -19,7 +19,7 @@ sap.ui.define(['./SemanticToggleButton'], function(SemanticToggleButton) {
 	* @extends sap.f.semantic.SemanticToggleButton
 	*
 	* @author SAP SE
-	* @version 1.52.41
+	* @version 1.52.42
 	*
 	* @constructor
 	* @public
--- resources/sap/f/semantic/SemanticToggleButton.js
+++ resources/sap/f/semantic/SemanticToggleButton.js
@@ -27,7 +27,7 @@ sap.ui.define([
 	* @abstract
 	*
 	* @author SAP SE
-	* @version 1.52.41
+	* @version 1.52.42
 	*
 	* @constructor
 	* @public

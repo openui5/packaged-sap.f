015f4b820416c2097c78b8d4f50841e78a9630c1
OpenUI5 <openui5@sap.com>
2020-08-10 15:25:32 +0200

    1.52.43

--- resources/sap/f/semantic/EditAction.js
+++ resources/sap/f/semantic/EditAction.js
@@ -18,7 +18,7 @@ sap.ui.define(['./SemanticButton'], function(SemanticButton) {
 	* @extends sap.f.semantic.SemanticButton
 	*
 	* @author SAP SE
-	* @version 1.52.42
+	* @version 1.52.43
 	*
 	* @constructor
 	* @public
--- resources/sap/f/semantic/SendMessageAction.js
+++ resources/sap/f/semantic/SendMessageAction.js
@@ -19,7 +19,7 @@ sap.ui.define(['sap/f/semantic/SemanticButton'], function(SemanticButton) {
 	* @extends sap.f.semantic.SemanticButton
 	*
 	* @author SAP SE
-	* @version 1.52.42
+	* @version 1.52.43
 	*
 	* @constructor
 	* @public
